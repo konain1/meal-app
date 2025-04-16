e3d9264848dd56d80d971bb6b784312440ff27ba
Konain1 <kaunainkasab@gmail.com>
2025-04-16 16:24:21 +0530

    using docs react navigation is working

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStaticNavigation } from '@react-navigation/native';
 
+
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: CategoriesScreen,
+  },
+});
+
+
+const Navigation = createStaticNavigation(RootStack);
 export default function App() {
+
   return (
-    <View style={styles.container}>
-     <CategoriesScreen />
-    </View>
+    <>
+ <Navigation />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#241c15 ',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
